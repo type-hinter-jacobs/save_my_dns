7fb4bb70cafaa78cdd1cb98ecf5f95b6f0c5318e
type-hinter-jacobs <keanu.jacobs.012@gmail.com>
2025-12-18 15:54:48 +0200

    Update test_rules.py

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -1,1 +1,9 @@
+def test_blocked_domain_returns_block():
+    denylist = ["porn.com"]
+    domain = "porn.com"
+    assert True
 
+def test_unlisted_domain_returns_allow():
+    denylist = ["porn.com"]
+    domain = "google.com"
+    assert True
