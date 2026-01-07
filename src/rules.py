8055cb6ab6d7cb57e8139b0cd73ebda8d1dfede4
type-hinter-jacobs <keanu.jacobs.012@gmail.com>
2026-01-07 16:53:09 +0200

    feat: block subdomains of denylisted domains

--- src/rules.py
+++ src/rules.py
@@ -1,11 +1,16 @@
-# Rule implementation will go here
-
 def evaluate_domain(domain, denylist):
+  # convert list elements to lowercase and strip whitespaces
+  denylist = [domain.strip().lower() for domain in denylist]
   # convert string to all lowercase letters
   domain = domain.strip().lower()
-  # if domain present in the denylist list return BLOCK
-  if domain in denylist:
-    return "BLOCK"
-  #if domain not present in the denylist list return ALLOW
-  else:
-    return "ALLOW"
+
+  for blocked_domain in denylist:
+    # if domain present in the denylist list return BLOCK
+    if domain == blocked_domain:
+      return "BLOCK"
+    # if domain of subdomain is present in the denylist list return BLOCK
+    else if domain.endswith("." + blocked_domain):
+      return "BLOCK"
+    # if domain not present in the denylist list return ALLOW
+    else:
+      return "ALLOW"
