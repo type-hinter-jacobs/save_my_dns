6c96f891339f26fb1d02f65d549864d0ba187920
type-hinter-jacobs <keanu.jacobs.012@gmail.com>
2026-01-07 16:11:56 +0200

    feat: normalise domain for case-insensitive matching

--- src/rules.py
+++ src/rules.py
@@ -1,6 +1,8 @@
 # Rule implementation will go here
 
 def evaluate_domain(domain, denylist):
+  # convert string to all lowercase letters
+  domain = domain.lower()
   # if domain present in the denylist list return BLOCK
   if domain in denylist:
     return "BLOCK"
