7366e86265a9f2e701ee1a485b33930e0a02645f
Keanu <148855533+KJac0bs@users.noreply.github.com>
2026-02-17 12:30:15 +0200

    feat: create centralised location to wire API and DNS server to database

---
+++ src/wiring.py
@@ -0,0 +1,38 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from src.repository.denylist import SQLAlchemyDenylistRepository
+from src.models import Base
+
+
+DATABASE_URL = "sqlite:///data/save_my_dns.db"
+
+def init_db(engine):
+    """
+    - initialisation function that creates tables at startup
+    - can be called multiple times, safely
+    """
+    Base.metadata.create_all(engine)
+    return None
+
+def build_engine(db_url: str | None = None):
+    """
+    create and return a SQLAlchemy Engine bound to the passed database URL
+    """
+    if db_url is None:
+        db_url = DATABASE_URL
+    engine = create_engine(url=db_url)
+    return engine
+
+def build_session_factory(engine):
+    """
+    create and return a SQLAlchemy session factory bound to the passed engine
+    """
+    session_factory = sessionmaker(bind=engine)
+    return session_factory
+
+
+def build_repo(session_factory):
+    """
+    create and return a SQLAlchemyDenylistRepository object using the passed session factory
+    """
+    return SQLAlchemyDenylistRepository(session_factory)
