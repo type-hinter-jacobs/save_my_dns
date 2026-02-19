66424b22a36d2fdec5d994e16df18971e9813ac4
Keanu <148855533+KJac0bs@users.noreply.github.com>
2026-02-19 14:35:52 +0200

    config: load database url from environment

--- src/wiring.py
+++ src/wiring.py
@@ -1,5 +1,3 @@
-from dotenv import load_dotenv
-load_dotenv()
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from src.repository.denylist import SQLAlchemyDenylistRepository
